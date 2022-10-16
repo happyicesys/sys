7b573e0ebb86a806ec0bbefdd0b1fa36a3ecbe0a
Brian <brian@Brians-MacBook-Pro.local>
2022-10-16 22:58:13 +0800

    debug

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::match(['get', 'post'], '/v1/vend-data', [VendDataController::class, 'create']);
+Route::post('/v1/vend-data', [VendDataController::class, 'create']);
