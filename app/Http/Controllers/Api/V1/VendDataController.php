d3d4d3824aeeebbd57e61ddda65e039a2eb8f090
Brian <brian@Brians-MacBook-Pro.local>
2022-10-19 22:58:12 +0800

    log info

--- app/Http/Controllers/Api/V1/VendDataController.php
+++ app/Http/Controllers/Api/V1/VendDataController.php
@@ -14,12 +14,14 @@ use App\Models\VendTransaction;
 use App\Jobs\ProcessVendData;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Log;
 
 class VendDataController extends Controller
 {
     public function create(Request $request)
     {
         $input = $request->all();
+        Log::info($input);
 
         ProcessVendData::dispatch($input);
 
