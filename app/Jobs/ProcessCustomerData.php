f5e8d7a1559da6d21b248f52c2392bb454b47ddb
Brian <brian@Brians-MacBook-Pro.local>
2022-12-09 18:25:21 +0800

    customer data debug

--- app/Jobs/ProcessCustomerData.php
+++ app/Jobs/ProcessCustomerData.php
@@ -303,7 +303,9 @@ class ProcessCustomerData implements ShouldQueue
 
                 if($customerCollection['is_dvm'] or $customerCollection['is_vending'] or $customerCollection['is_combi']) {
                     $vend = Vend::where('code', $customerCollection['vend_code'])->first();
-                    $vend->vendBindings()->update(['is_active' => false]);
+                    if($vend->vendBindings()->exists()) {
+                        $vend->vendBindings()->update(['is_active' => false]);
+                    }
 
                     if($vend) {
                         $customer->vendBinding()->updateOrCreate([
