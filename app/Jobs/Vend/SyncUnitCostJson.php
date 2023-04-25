1822e95da4ffe400014368a89efa889267f57639
Brian <brian@Brians-MacBook-Pro.local>
2023-04-25 22:17:24 +0800

    revenue and unit cost column vend transactions

--- app/Jobs/Vend/SyncUnitCostJson.php
+++ app/Jobs/Vend/SyncUnitCostJson.php
@@ -37,7 +37,7 @@ class SyncUnitCostJson implements ShouldQueue
         $vendTransaction->update([
             'revenue' => round($revenue),
             'gross_profit' => round($grossProfit),
-            'gross_profit_margin' => ($grossProfit/ $revenue),
+            'gross_profit_margin' => ($grossProfit/ $revenue * 100),
             'unit_cost' => $unitCost,
             'unit_cost_json' => [
                 'amount' => $vendTransaction->amount,
