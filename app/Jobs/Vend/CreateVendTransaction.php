9be8478d675a9dd1705c5c93271321435a4bc6f5
Brian <brian@Brians-MacBook-Pro.local>
2023-05-03 01:36:01 +0800

    debug report

--- app/Jobs/Vend/CreateVendTransaction.php
+++ app/Jobs/Vend/CreateVendTransaction.php
@@ -20,8 +20,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class CreateVendTransaction
-//implements ShouldQueue
+class CreateVendTransaction implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
@@ -100,9 +99,9 @@ class CreateVendTransaction
         // check duplicated orderid
         $duplicatedOrderId = VendTransaction::where('order_id', $processedInput['orderId'])->where('vend_id', $vend->id)->first();
 
-        // if($duplicatedOrderId) {
-        //     return;
-        // }
+        if($duplicatedOrderId) {
+            return;
+        }
 
         $vendTransaction = VendTransaction::create([
             'transaction_datetime' => Carbon::now(),
