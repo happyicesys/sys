944fc54d02f425c496878c7fa747f2c4f3570105
Brian <brian@Brians-MBP.realtek>
2023-05-11 18:19:27 +0800

    make omise live

--- app/Services/PaymentGatewayService.php
+++ app/Services/PaymentGatewayService.php
@@ -73,11 +73,11 @@ class PaymentGatewayService
   {
     $envName = 'sandbox';
 
-    // if(config('app.env') === 'production') {
-    //   $envName = 'production';
-    // }else {
-    //   $envName = 'sandbox';
-    // }
+    if(config('app.env') === 'production') {
+      $envName = 'production';
+    }else {
+      $envName = 'sandbox';
+    }
 
     return $envName;
   }
