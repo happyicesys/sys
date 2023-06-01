cedf4ac2d82c2ddfed458fe63baba342f3aa35aa
happyicesys <brianlee@happyice.com.my>
2023-06-01 20:58:25 +0800

    debug

--- app/Jobs/Vend/SyncVendChannels.php
+++ app/Jobs/Vend/SyncVendChannels.php
@@ -14,8 +14,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class SyncVendChannels
-//implements ShouldQueue
+class SyncVendChannels implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
