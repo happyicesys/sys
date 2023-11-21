89bc3e30517e1150898190a85d9b6cbb7d0182b5
brian <leehongjie91@gmail.com>
2023-11-21 15:25:08 +0800

    qos1

--- app/Http/Controllers/ProductMappingController.php
+++ app/Http/Controllers/ProductMappingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\Vend\SaveVendChannelsJson;
 use App\Http\Resources\ProductResource;
 use App\Http\Resources\ProductMappingResource;
 use App\Http\Resources\VendResource;
