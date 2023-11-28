014c4b858503c4a4dda8954f550715bdff47e0b2
brian <leehongjie91@gmail.com>
2023-11-28 14:56:23 +0800

    debug date

--- app/Jobs/DispenseDeliveryPlatformOrder.php
+++ app/Jobs/DispenseDeliveryPlatformOrder.php
@@ -11,8 +11,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class DispenseDeliveryPlatformOrder
-//implements ShouldQueue
+class DispenseDeliveryPlatformOrder implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
