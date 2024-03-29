b7bd0fb370027d72dbbcd9bce33a28a23a969ad9
brian <leehongjie91@gmail.com>
2024-03-30 00:33:11 +0800

    remove virtual apk

--- app/Models/Vend.php
+++ app/Models/Vend.php
@@ -83,7 +83,6 @@ class Vend extends Model
         'vend_criteria_weightage_json',
         'vend_transaction_totals_json',
         'vend_type_id',
-        'virtual_apk_ver',
         'virtual_firmware_ver',
     ];
 
