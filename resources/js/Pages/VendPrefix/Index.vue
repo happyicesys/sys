e395c34689e9b6fb1f8ca409980b6a4f41ad6109
happyicesys <brianlee@happyice.com.my>
2024-06-20 10:07:09 +0800

    debug vend prefix

--- resources/js/Pages/VendPrefix/Index.vue
+++ resources/js/Pages/VendPrefix/Index.vue
@@ -155,7 +155,7 @@
                             type="button"
                             class="bg-red-300 hover:bg-red-400 px-3 py-2 text-xs text-red-800 flex-col space-y-1 w-fit"
                             :class="[(vendPrefix.vends && vendPrefix.vends.length > 0) || (vendPrefix.vendConfigs && vendPrefix.vendConfigs.length > 0) ? 'opacity-50 cursor-not-allowed' : '']"
-                            @click="onDeleteClicked(vendModel)"
+                            @click="onDeleteClicked(vendPrefix)"
                             :disabled="(vendPrefix.vends && vendPrefix.vends.length > 0) || (vendPrefix.vendConfigs && vendPrefix.vendConfigs.length > 0)"
                           >
                             <span class="flex space-x-1 items-center">
