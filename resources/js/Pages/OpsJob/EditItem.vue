dc55b18cc25b71271d9c493df2dd207556e1e84e
happyicesys <brianlee@happyice.com.my>
2024-08-27 14:37:02 +0800

    ops job item sequence

--- resources/js/Pages/OpsJob/EditItem.vue
+++ resources/js/Pages/OpsJob/EditItem.vue
@@ -3,6 +3,9 @@
   <BreezeAuthenticatedLayout>
     <template #header >
       <div class="flex flex-col space-y-1 md:flex-row md:space-x-3 md:space-y-0 text-left">
+          <span class="font-semibold">
+            ({{ opsJobItem.sequence }})
+          </span>
           <span>
             Job ID#
             <span class="text-blue-800">
